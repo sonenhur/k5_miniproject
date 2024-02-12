f323b786bbef28be5c3c9375d65a315903c5519b
sonenhur <148170713+sonenhur@users.noreply.github.com>
2024-02-12 22:26:35 +0900

    데이터베이스 설계 - 테이블

--- movie_db.sql
+++ movie_db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE movie(
-    movie_cd INT AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT AUTO_INCREMENT PRIMARY KEY,
     movie_title VARCHAR(100),
     release_date DATE,
     running_time INT,
@@ -10,6 +10,40 @@ CREATE TABLE movie(
     casts VARCHAR(100)	
 );
 
+CREATE TABLE screening_schedule( 
+    screening_id INT AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT,
+    screening_date DATE,
+    theater_id INT,
+    FOREIGN KEY(movie_id) REFERENCES movie(movie_id),
+    FOREIGN KEY(theater_id) REFERENCES theater(theater_id)
+);
+
+CREATE TABLE cinema( 
+    cinema_id INT PRIMARY KEY, 
+    cinema_name VARCHAR(15),
+    cinema_location VARCHAR(100),
+    parking_info VARCHAR(100), 
+    cinema_phone VARCHAR(15)
+);
+
+CREATE TABLE theater( 
+    theater_id INT PRIMARY KEY,
+    cinema_id INT,
+    theater_no INT,
+    theater_type VARCHAR(20),
+    FOREIGN KEY(cinema_id) REFERENCES cinema(cinema_id)
+);
+
+CREATE TABLE seat( 
+    seat_id INT AUTO_INCREMENT PRIMARY KEY,
+    theater_id INT,
+    seat_row_no CHAR(1),
+    seat_col_no INT,
+    FOREIGN KEY(theater_id) REFERENCES theater(theater_id)
+);
+
+
 CREATE TABLE member( 
     mem_id VARCHAR(50) PRIMARY KEY,
     mem_pw VARCHAR(2000),
@@ -19,65 +53,32 @@ CREATE TABLE member(
     join_date DATE
 );
 
-CREATE TABLE review( 
-    mem_id VARCHAR(50),
-    movie_cd INT,
-    review_date DATE,
-    grade DECIMAL(3,2),
-    review_contents VARCHAR(300),
-    FOREIGN KEY(mem_id) REFERENCES member(mem_id),
-    FOREIGN KEY(movie_cd) REFERENCES movie(movie_cd)
-);
-
-CREATE TABLE screening_schedule( 
-    screening_cd INT AUTO_INCREMENT PRIMARY KEY,
-    movie_cd INT,
-    screening_date DATE,
-    theater_cd INT,
-    FOREIGN KEY(movie_cd) REFERENCES movie(movie_cd),
-    FOREIGN KEY(theater_cd) REFERENCES theater(theater_cd)
-);
-
-CREATE TABLE theater( 
-    theater_cd INT PRIMARY KEY,
-    cinema_cd INT,
-    theater_no INT,
-    theater_type VARCHAR(20),
-    FOREIGN KEY(cinema_cd) REFERENCES cinema(cinema_cd)
-);
-
-CREATE TABLE cinema( 
-    cinema_cd INT PRIMARY KEY, 
-    cinema_name VARCHAR(15),
-    cinema_location VARCHAR(100),
-    parking_info VARCHAR(100), 
-    cinema_phone VARCHAR(15)
-);
-
-CREATE TABLE seat( 
-    seat_cd INT AUTO_INCREMENT PRIMARY KEY,
-    theater_cd INT,
-    seat_row_no CHAR(1),
-    seat_col_no INT,
-    FOREIGN KEY(theater_cd) REFERENCES theater(theater_cd)
-);
-
 CREATE TABLE ticketing( 
-    ticket_cd INT AUTO_INCREMENT PRIMARY KEY,
+    ticket_id INT AUTO_INCREMENT PRIMARY KEY,
     mem_id VARCHAR(50),
     adult INT,
     teenager INT,
     child INT,
-    screening_cd INT,
+    screening_id INT,
     FOREIGN KEY(mem_id) REFERENCES member(mem_id),
-    FOREIGN KEY(screening_cd) REFERENCES screening_schedule(screening_cd)
+    FOREIGN KEY(screening_id) REFERENCES screening_schedule(screening_id)
 );
 
 CREATE TABLE payment(
     payment_id INT AUTO_INCREMENT PRIMARY KEY,
-    ticket_cd INT,
+    ticket_id INT,
     payment_date DATE,
     payment_method VARCHAR(50),
     amount INT,
-    FOREIGN KEY(ticket_cd) REFERENCES ticketing(ticket_cd)
+    FOREIGN KEY(ticket_id) REFERENCES ticketing(ticket_id)
+);
+
+CREATE TABLE review( 
+    mem_id VARCHAR(50),
+    movie_id INT,
+    review_date DATE,
+    grade DECIMAL(3,2),
+    review_contents VARCHAR(300),
+    FOREIGN KEY(mem_id) REFERENCES member(mem_id),
+    FOREIGN KEY(movie_id) REFERENCES movie(movie_id)
 );
