8c394af30a9ced1e00351aa28d97980c2cfaaebc
sonenhur <148170713+sonenhur@users.noreply.github.com>
2024-02-15 22:43:04 +0900

    DB 생성코드 (MySQL)

--- movie_db.sql
+++ movie_db.sql
@@ -1,31 +1,16 @@
-CREATE TABLE cinema( 
-    cinema_id INT PRIMARY KEY, 
-    cinema_name VARCHAR(15),
-    cinema_location VARCHAR(100),
-    cinema_phone VARCHAR(15)
-);
-
-CREATE TABLE theater( 
-    theater_id INT PRIMARY KEY,
-    cinema_id INT,
-    theater_no INT,
-    theater_type VARCHAR(20),
-    FOREIGN KEY(cinema_id) REFERENCES cinema(cinema_id)
-);
-
-CREATE TABLE movie(
+CREATE TABLE movie (
     movie_id INT PRIMARY KEY,
-    movie_title VARCHAR(100),
+    title VARCHAR(100),
     release_date DATE,
-    running_time VARCHAR(50),
-    film_rate VARCHAR(50),
+    running_time​ VARCHAR(50),
+    rating VARCHAR(50),
     genre VARCHAR(50),
     synopsis VARCHAR(2000),
     director VARCHAR(50),
-    casts VARCHAR(100)	
+    cast VARCHAR(100)	
 );
 
-CREATE TABLE screening_schedule( 
+CREATE TABLE screening ( 
     screening_id INT PRIMARY KEY,
     movie_id INT,
     screening_date DATE,
@@ -36,49 +21,63 @@ CREATE TABLE screening_schedule(
     FOREIGN KEY(theater_id) REFERENCES theater(theater_id)
 );
 
-CREATE TABLE seat( 
+CREATE TABLE cinema ( 
+    cinema_id INT PRIMARY KEY, 
+    name VARCHAR(15),
+    location VARCHAR(100),
+    phone VARCHAR(15)
+);
+
+CREATE TABLE theater ( 
+    theater_id INT PRIMARY KEY,
+    cinema_id INT,
+    theater_no INT,
+    theater_type VARCHAR(20),
+    FOREIGN KEY(cinema_id) REFERENCES cinema(cinema_id)
+);
+
+CREATE TABLE seat ( 
     seat_id INT PRIMARY KEY,
     theater_id INT,
-    seat_row_no CHAR(1),
-    seat_col_no INT,
+    seat_row CHAR(1),
+    seat_col INT,
     FOREIGN KEY(theater_id) REFERENCES theater(theater_id)
 );
 
-CREATE TABLE member( 
-    member_id VARCHAR(50) PRIMARY KEY,
-    member_pw VARCHAR(2000),
-    member_birth DATE,
-    member_address VARCHAR(100),
-    member_phone VARCHAR(15),
+CREATE TABLE member ( 
+    email VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(2000),
+    name VARCHAR(50),
+    birth DATE,
     join_date DATE
 );
 
-CREATE TABLE ticketing( 
-    ticket_id INT PRIMARY KEY,
-    member_id VARCHAR(50),
+CREATE TABLE reservation ( 
+    reservation_id INT PRIMARY KEY,
+    email VARCHAR(50),
     adult INT,
     teenager INT,
     child INT,
     screening_id INT,
-    FOREIGN KEY(member_id) REFERENCES member(member_id),
-    FOREIGN KEY(screening_id) REFERENCES screening_schedule(screening_id)
+    FOREIGN KEY(email) REFERENCES member(email),
+    FOREIGN KEY(screening_id) REFERENCES screening(screening_id)
 );
 
-CREATE TABLE payment(
+CREATE TABLE payment (
     payment_id INT PRIMARY KEY,
-    ticket_id INT,
+    reservation_id INT,
     payment_date DATE,
     payment_method VARCHAR(50),
     amount INT,
-    FOREIGN KEY(ticket_id) REFERENCES ticketing(ticket_id)
+    FOREIGN KEY(reservation_id) REFERENCES reservation(reservation_id)
 );
 
-CREATE TABLE review( 
-    member_id VARCHAR(50),
+CREATE TABLE review ( 
+    email VARCHAR(50),
     movie_id INT,
     review_date DATE,
-    grade DECIMAL(3,2),
-    review_contents VARCHAR(300),
-    FOREIGN KEY(member_id) REFERENCES member(member_id),
+    rating DECIMAL(3,2),
+    content VARCHAR(300),
+    FOREIGN KEY(email) REFERENCES member(email),
     FOREIGN KEY(movie_id) REFERENCES movie(movie_id)
 );
